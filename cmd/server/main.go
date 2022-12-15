481f21d43eb1b9146c0cb1933c7b90346ec8d9d2
Bruno Barbosa <brb.dev@outlook.com>
2022-12-15 19:18:39 -0400

    adiciona rotas /dentistas a main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/ALTbruno/consultorio-golang/cmd/server/controller"
 	"github.com/ALTbruno/consultorio-golang/config"
 	"github.com/ALTbruno/consultorio-golang/db"
 	"github.com/gin-gonic/gin"
@@ -12,6 +13,17 @@ func init() {
 }
 
 func main() {
+	
 	app := gin.Default()
+
+	dentistas := app.Group("/dentistas")
+	{
+		dentistas.POST("", controller.DentistaPOST)
+		dentistas.GET("/:id", controller.DentistaGET)
+		dentistas.PUT("/:id", controller.DentistaPUT)
+		dentistas.PATCH("/:id", controller.DentistaPATCH)
+		dentistas.DELETE("/:id", controller.DentistaDELETE)
+	}
+
 	app.Run()
 }
