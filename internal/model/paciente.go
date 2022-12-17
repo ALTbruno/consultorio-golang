ea19bb8101d33c53ef0ac7bf24314d8d50c702c9
Bruno Barbosa <brb.dev@outlook.com>
2022-12-17 19:03:35 -0400

    atualiza model paciente

--- internal/model/paciente.go
+++ internal/model/paciente.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Paciente struct {
 	gorm.Model
-	Nome           string   `json:"nome"`
-	Sobrenome      string   `json:"sobrenome"`
-	RG             string   `json:"rg"`
+	Nome           string   `json:"nome" gorm:"not null" validate:"required"`
+	Sobrenome      string   `json:"sobrenome" gorm:"not null" validate:"required"`
+	RG             string   `json:"rg" gorm:"not null" validate:"required"`
 	DataDeCadastro string   `json:"dataDeCadastro"`
 	Consulta       Consulta `json:"-"`
 }
