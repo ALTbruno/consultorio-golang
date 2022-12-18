7c02ad9ea60710c1e21e05ea89d1c5b5a1504cd1
Bruno Barbosa <brb.dev@outlook.com>
2022-12-17 21:42:52 -0400

    atualiza model consulta

--- internal/model/consulta.go
+++ internal/model/consulta.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type Consulta struct {
 	gorm.Model
-	DentistaID uint   `json:"id_dentista"`
-	PacienteID uint   `json:"id_paciente"`
-	DataHora   string `json:"dataHora"`
-	Descricao  string `json:"descricao"`
+	DentistaID uint   `json:"id_dentista" gorm:"not null" validate:"required"`
+	PacienteID uint   `json:"id_paciente" gorm:"not null" validate:"required"`
+	DataHora   string `json:"dataHora" gorm:"not null" validate:"required"`
+	Descricao  string `json:"descricao" gorm:"not null" validate:"required"`
 }
 
 type Tabler interface {
