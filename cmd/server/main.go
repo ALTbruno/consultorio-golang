f080a31cad4ddc663e9a0f6946e8a335a2220b50
Bruno Barbosa <brb.dev@outlook.com>
2022-12-18 18:21:53 -0400

    adiciona anotacoes doc global

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ALTbruno/consultorio-golang/cmd/server/controller"
+	"github.com/ALTbruno/consultorio-golang/cmd/server/docs"
 	"github.com/ALTbruno/consultorio-golang/config"
 	"github.com/ALTbruno/consultorio-golang/db"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
 )
 
 func init() {
@@ -12,9 +18,25 @@ func init() {
 	db.ConnectToDB()
 }
 
+// @title Cosultório Odontológico
+// @version 1.0
+// @description API responsável pelo gerenciamento de Dentistas, Pacientes e Consultas
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 
 	app := gin.Default()
+
+	apiHost := os.Getenv("HOST")
+	apiPort := os.Getenv("PORT")
+	host := fmt.Sprintf("%s:%s", apiHost, apiPort)
+	docs.SwaggerInfo.Host = host
+
+	app.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	dentistas := app.Group("/dentistas")
 	{
