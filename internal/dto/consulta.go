54883eb3e90c50ffaf43a4c4772723bae2156028
Bruno Barbosa <brb.dev@outlook.com>
2022-12-18 18:56:08 -0400

    atualiza e adiciona dtos consulta

--- internal/dto/consulta.go
+++ internal/dto/consulta.go
@@ -1,8 +1,23 @@
 package dto
 
-type Consulta struct {
-	DentistaID uint   `json:"id_dentista" gorm:"not null" validate:"required"`
-	PacienteID uint   `json:"id_paciente" gorm:"not null" validate:"required"`
+type ConsultaBody struct {
+	DentistaID uint   `json:"id_dentista" gorm:"not null" validate:"required" example:"3"`
+	PacienteID uint   `json:"id_paciente" gorm:"not null" validate:"required" example:"19"`
 	DataHora   string `json:"dataHora" gorm:"not null" validate:"required" example:"23-02-2021 14:30"`
 	Descricao  string `json:"descricao" gorm:"not null" validate:"required" example:"Limpeza e extração"`
+}
+
+type ConsultaBodyMatriculaRG struct {
+	MatriculaDentista string `json:"matricula_dentista" gorm:"not null" validate:"required" example:"C123"`
+	RGPaciente        string `json:"rg_paciente" gorm:"not null" validate:"required" example:"789456"`
+	DataHora          string `json:"dataHora" gorm:"not null" validate:"required" example:"23-02-2021 14:30"`
+	Descricao         string `json:"descricao" gorm:"not null" validate:"required" example:"Limpeza e extração"`
+}
+
+type ConsultaResponse struct {
+	ID         uint   `json:"id" gorm:"not null" validate:"required" example:"50"`
+	DentistaID uint   `json:"id_dentista" gorm:"not null" validate:"required" example:"3"`
+	PacienteID uint   `json:"id_paciente" gorm:"not null" validate:"required"  example:"19"`
+	DataHora   string `json:"dataHora" gorm:"not null" validate:"required" example:"23-02-2021 14:30"`
+	Descricao  string `json:"descricao" gorm:"not null" validate:"required"  example:"Limpeza e extração"`
 }
